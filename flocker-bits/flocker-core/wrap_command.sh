11cc725948c378db905cfd7020f9f370329a91e5
Kai Davenport <kaiyadavenport@gmail.com>
2015-08-19 01:27:24 +0200

    need to escape $@

--- flocker-bits/flocker-core/wrap_command.sh
+++ flocker-bits/flocker-core/wrap_command.sh
@@ -20,7 +20,7 @@ mode=$3
 cat > ${dir}/ns${cmd} <<EOF
 #!/bin/bash
 
-cmd="/bin/nsenter --mount=/host/proc/1/ns/mnt -- ${dir}/${cmd} \"$@\"
+cmd="/bin/nsenter --mount=/host/proc/1/ns/mnt -- ${dir}/${cmd} \"\$@\"
 
 if [ -n $DEBUG ]; then
 echo $cmd >> /tmp/flocker-command-log
