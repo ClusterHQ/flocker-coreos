afba1fb5649c97dcc69ada40d728af4a86535a71
Dennis Benkert <spinecrasher@googlemail.com>
2015-10-05 14:19:14 +0200

    added debug flags for mtab wrapper script

--- flocker-bits/dataset-agent/wrap_dataset_agent_mtab.sh
+++ flocker-bits/dataset-agent/wrap_dataset_agent_mtab.sh
@@ -6,14 +6,23 @@
 # prime it, before we go off into the backgrounded subshell
 unlink /etc/mtab
 /bin/nsenter --mount=/host/proc/1/ns/mnt -- cat /etc/mtab > /etc/mtab
->&2 echo "primed mtab"
+
+if [[ ${DEBUG} = "1" ]]
+then
+  >&2 echo "primed mtab"
+fi
 
 # update mtab atomically every second
 (
     while true; do
         /bin/nsenter --mount=/host/proc/1/ns/mnt -- cat /etc/mtab > /etc/mtab.tmp
         mv /etc/mtab.tmp /etc/mtab
-        >&2 echo "updated mtab"
+
+        if [[ ${DEBUG} = "1" ]]
+        then
+          >&2 echo "updated mtab"
+        fi
+
         sleep 1
     done
 ) &
