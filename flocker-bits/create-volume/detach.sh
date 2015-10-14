cac336383e7a3d27433be0b670fcf726b4b308d1
Kai Davenport <kaiyadavenport@gmail.com>
2015-10-14 16:13:37 +0000

    example script for detaching a dataset without deleting it

---
+++ flocker-bits/create-volume/detach.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+sudo docker build -t clusterhq/create-volume:1.2.0-1rev1 .
+sudo docker run -ti --rm \
+    --volumes-from certs \
+    -e FLOCKER_API_CERT_NAME=plugin \
+    -e FLOCKER_CONTROL_SERVICE_ENDPOINT=10.154.243.239 \
+    clusterhq/create-volume:1.2.0-1rev1 \
+    --dataset-uuid 11111111-1111-1111-1111-111111111131 \
+    --host-uuid none
+
