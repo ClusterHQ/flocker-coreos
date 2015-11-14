f3ceb1bb803c2adf62d963b5ad4ae62af8b52c65
Richard Wall <richard.wall@clusterhq.com>
2015-11-13 16:14:28 -0800

    Add some mysql commands

--- flocker-bits/demo_datasets.sh
+++ flocker-bits/demo_datasets.sh
@@ -30,6 +30,39 @@ $CURL https://172.20.20.102:4523/v1/configuration/datasets | jq '.'
 $CURL https://172.20.20.102:4523/v1/state/datasets | jq '.'
 read -p "Enter Dataset UUID: " DATASET_UUID
 
+# Start Mysql container on 102
+ssh core@172.20.20.102
+
+lsblk
+
+read -p "Enter Dataset path: " DATASET_PATH
+
+docker run \
+    --interactive \
+    --tty \
+    --rm  \
+    --publish-all \
+    --volume "${DATASET_PATH}/data:/var/lib/mysql" \
+    --env MYSQL_ROOT_PASSWORD=secret \
+    mysql:latest
+
+# Discover remote port
+ssh core@172.20.20.102 docker ps | grep mysql
+
+# Connect to MySQL
+mysql --host 172.20.20.102 --port 32770 --user root --password
+
+# Create database
+create database salesforce1;
+
+# Stop database
+ssh core@172.20.20.102 docker ps | grep mysql
+
+# List converged dataset state
+$CURL https://172.20.20.102:4523/v1/state/datasets | jq '.'
+read -p "Enter Dataset UUID: " DATASET_UUID
+
+
 # Move dataset to NODE2
 $CURL https://172.20.20.102:4523/v1/state/nodes | jq '.'
 read -p "Enter NODE2 UUID: " NODE2_UUID
@@ -44,3 +77,28 @@ EOF
 
 # List converged dataset state
 $CURL https://172.20.20.102:4523/v1/state/datasets | jq '.'
+
+
+# Start Mysql container on 103
+ssh core@172.20.20.103
+
+lsblk
+
+read -p "Enter Dataset path: " DATASET_PATH
+
+docker run \
+    --interactive \
+    --tty \
+    --rm  \
+    --publish-all \
+    --volume "${DATASET_PATH}/data:/var/lib/mysql" \
+    --env MYSQL_ROOT_PASSWORD=secret \
+    mysql:latest
+
+# Discover remote port
+ssh core@172.20.20.103 docker ps | grep mysql
+
+read -p "Enter MySQL port: " MYSQL_PORT
+
+# Connect to MySQL
+mysql --host 172.20.20.103 --port "${MYSQL_PORT}" --user root --password
