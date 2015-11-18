ea501381e80339da10548f416526cefe138b2f77
Richard Wall <richard.wall@clusterhq.com>
2015-11-17 16:37:09 -0800

    Install flocker-1.7.2-1 and tag the Docker images as 1.7.2

--- flocker-bits/buildimages.sh
+++ flocker-bits/buildimages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-FLOCKER_TAG=1.5.0-1rev1
+set -e
+export FLOCKER_TAG=1.7.2
 docker build -t clusterhq/flocker-core:$FLOCKER_TAG flocker-core
 docker build -t clusterhq/flocker-container-agent:$FLOCKER_TAG container-agent
 docker build -t clusterhq/flocker-control-service:$FLOCKER_TAG control-agent
